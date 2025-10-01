79fee0e079839b277a7514691715d8df1ef5f6de
Akash Naskar <akash.buban2@gmail.com>
2025-10-02 03:07:10 +0530

    added config_loader and model_loader

---
+++ logger/__init__.py
@@ -0,0 +1,8 @@
+# --- Usage Example ---
+# from logger.custom_logger import CustomLogger
+
+# if __name__ == "__main__":
+#     logger = CustomLogger().get_logger(__file__)
+#     logger.info("User uploaded a file", user_id=123, filename="report.pdf")
+#     logger.error("Failed to process PDF", error="File not found", user_id=123)
+## working as expected
